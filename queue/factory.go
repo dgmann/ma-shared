3066ab30b32fa4cc4321380b628bc3a675dee1ee
David Gaßmann <david.gassmann@gmail.com>
2017-08-31 16:09:55 +0200

    Added function to get queue config from env variables

--- queue/factory.go
+++ queue/factory.go
@@ -1,6 +1,9 @@
 package queue
 
-import "github.com/streadway/amqp"
+import (
+	"github.com/streadway/amqp"
+	"github.com/dgmann/ma-shared"
+)
 
 type Factory struct {
 	conn *amqp.Connection
@@ -25,6 +28,21 @@ func DefaultConfig() FactoryConfig {
 		Port: "5672",
 	}
 }
+
+func QueueConfigFromEnv() FactoryConfig {
+	user := shared.GetEnvOrDefault("QUEUE_USER", "guest")
+	password := shared.GetEnvOrDefault("QUEUE_PASSWORD", "guest")
+	host := shared.GetEnvOrDefault("QUEUE_HOST", "queue")
+	port := shared.GetEnvOrDefault("QUEUE_PORT", "5672")
+
+	return FactoryConfig{
+		User: user,
+		Password: password,
+		Host: host,
+		Port: port,
+	}
+}
+
 
 func NewFactory(config FactoryConfig) (*Factory) {
 	conn, err := amqp.Dial(config.ToConnectionString())
