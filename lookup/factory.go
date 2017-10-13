ed2206430f649d37fc3c06d91531e966fb737745
David Gaßmann <david.gassmann@gmail.com>
2017-10-13 11:29:09 +0200

    Added wrapper for lookup

---
+++ lookup/factory.go
@@ -0,0 +1,46 @@
+package lookup
+
+import (
+	"github.com/dgmann/ma-shared"
+	"sync"
+)
+
+type LookupFactory struct {
+	client StorageClient
+	numWorker int
+}
+
+func NewLookupFactory(client StorageClient, numWorker int) *LookupFactory {
+	return &LookupFactory{client:client, numWorker:numWorker}
+}
+
+func(factory *LookupFactory) StartLookupServices(input <-chan shared.Message) chan shared.Message {
+	var wg sync.WaitGroup
+	output := make(chan shared.Message, 10000)
+	for i:=0; i < factory.numWorker; i++ {
+		wg.Add(1)
+		go factory.lookup(input, output, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(output)
+	}()
+
+	return output
+}
+
+func(factory *LookupFactory) lookup(input <-chan shared.Message, output chan<-shared.Message, wg *sync.WaitGroup) {
+	for msg := range input {
+		msg.EnterStage("Lookup")
+		for _, result := range msg.Result.OpenALPR.Results {
+			exists := factory.client.Exists(result.Plate)
+			if exists {
+				msg.Result.WantedNumbers = append(msg.Result.WantedNumbers, result.Plate)
+			}
+		}
+		msg.LeaveStage("Lookup")
+		output <- msg
+	}
+	wg.Done()
+}
