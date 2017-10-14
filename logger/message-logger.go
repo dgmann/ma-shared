4283ecf2bca721c91ec4c147721f213cb93a0b93
David Gaßmann <david.gassmann@gmail.com>
2017-10-14 11:10:10 +0200

    Improved output formating of wantedplate logger

--- logger/message-logger.go
+++ logger/message-logger.go
@@ -52,9 +52,13 @@ func(logger *MessageLogger) PrintFoundPlates() chan shared.Message {
 func(logger *MessageLogger) PrintWanted() chan shared.Message {
 	output := make(chan shared.Message, 10000)
 	go func() {
+		lastCount := 0
 		for msg := range logger.input {
 			output <- msg
-			fmt.Printf("%+v\n", msg.Result.WantedNumbers)
+			if len(msg.Result.WantedNumbers) > lastCount {
+				fmt.Printf("Wanted Numbers:\n%+v\n", msg.Result.WantedNumbers)
+				lastCount = len(msg.Result.WantedNumbers)
+			}
 		}
 		close(output)
 	}()
